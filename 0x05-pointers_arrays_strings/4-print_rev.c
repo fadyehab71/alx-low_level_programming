4ef53ba8942f059c1013c4ef266e181c1182cb8d
Fady Ehab <fady.ehab@gmail.com>
2023-02-27 18:00:38 -0800

    afsd

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,18 +3,20 @@
  * print_rev - a function that prints a string, in reverse,
  * followed by a new line.
  * @s: an input string
- * Return: Nothing
- 
+ * Return: Nothing 
  */
 void print_rev(char *s)
 {
-	int len = 0;
+	char *len = s;
 
- 	while (s[len] != '\0')
+	while (*len)
 		len++;
+	len -= 1;
 
 	while (len)
-		_putchar(s[--len]);
-
+	{
+		_putchar(*s);
+		len--;
+	}
 	_putchar('\n');
 }
